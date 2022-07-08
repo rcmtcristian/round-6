afab8c7fa2393e353940642ace346c7b2911f18c
cris <rcmtcristian@gmail.com>
2022-07-08 14:17:44 -0400

    made objects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
 
+const rightArrow = document.getElementById('right')
+const prevArrow = document.getElementById('left')
+const showcase = document.querySelector('.showcase');
+let fSlide;
+let LSlide;
+
+let projects = [ 
+  {
+    display:"./images/Tesla-Model-3-PNG-HD.png",
+    title: "Tesla Model 3 ",
+    subTitle:"Long Range",
+    price: "$ 57,990"
+  },
+  {
+    display:"./images/Tesla-Model-3-PNG-HD.png",
+    title: "Tesla Model 3 ",
+    subTitle:"Long Range",
+    price: "$ 57,990"
+  },
+  {
+    display:"./images/Tesla-Model-3-PNG-HD.png",
+    title: "Tesla Model 3 ",
+    subTitle:"Long Range",
+    price: "$ 57,990"
+  }
+  
+
+]
+
+projects.forEach(({display,title,subTitle,price},i) => {
+  const slide = document.createElement('div');
+  slide.classList.add('insideSlide');
+  if(i == 0){
+    fSlide = slide;
+    slide.classList.add('active')
+  }
+});
