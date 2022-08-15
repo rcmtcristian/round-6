625f94052559fdfac76d0f09c95b5b7f2a402aa7
rcmtcristian <rcmtcristian@gmail.com>
2022-08-15 14:50:55 -0400

    stuff

--- js/main.js
+++ js/main.js
@@ -10,17 +10,17 @@ let projects = [
     display:"./images/Tesla-Model-3-PNG-HD.png",
     title: "Tesla Model 3 ",
     subTitle:"Long Range",
-    price: "$ 57,990"
+    price: "$ 57,990",
   },
   {
     display:"./images/Tesla-Model-3-PNG-HD.png",
-    title: "Tesla Model 3 ",
+    title: "Tesla Model 2 ",
     subTitle:"Long Range",
     price: "$ 57,990"
   },
   {
     display:"./images/Tesla-Model-3-PNG-HD.png",
-    title: "Tesla Model 3 ",
+    title: "Tesla Model 1 ",
     subTitle:"Long Range",
     price: "$ 57,990"
   }
@@ -31,8 +31,17 @@ let projects = [
 projects.forEach(({display,title,subTitle,price},i) => {
   const slide = document.createElement('div');
   slide.classList.add('insideSlide');
+  slide.style.backgroundImage  = `url(${display})`;
   if(i == 0){
     fSlide = slide;
     slide.classList.add('active')
   }
+
+  if(i + 1 == projects.length){
+    LSlide = slide
+  }
+
+
+  showcase.appendChild(slide)
 });
+
